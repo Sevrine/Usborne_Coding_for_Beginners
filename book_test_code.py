d5c507f4671673a2e8abc8b2640977b663b4389d
Sevrine <39972045+Sevrine@users.noreply.github.com>
2018-06-05 18:10:38 -0700

    added variables

--- book_test_code.py
+++ book_test_code.py
@@ -8,3 +8,11 @@ print(2 * 2)
 print(2 / 2)
 
 # Variables
+bananas = 5
+bananasEaten = 2
+print(bananas - bananasEaten)
+bananas = 10
+print(bananas - bananasEaten)
+exampleString = "I am a string."
+name = input("What is your name? ")
+print(name)
